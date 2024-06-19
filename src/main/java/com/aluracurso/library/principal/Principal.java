a524c60d8a6ddb50eb73c1ec7c69892c494270eb
lfgarciaoc <luizafernandagarcia@gmail.com>
2024-06-19 14:19:42 -0500

    refactor(main): restructure main class, add showMenu method and fetch book data APi.

--- src/main/java/com/aluracurso/library/principal/Principal.java
+++ src/main/java/com/aluracurso/library/principal/Principal.java
@@ -17,45 +17,80 @@ public class Principal {
     private final String URL_BASE = "https://gutendex.com/books/";
     private DataConverter converter = new DataConverter();
 
+
     public void showMenu(){
-        var json = apiRequest.getData(URL_BASE);
-        var data = converter.getData(json, Data.class);
-        System.out.println(data);
+        var option=-1;
+        while (option !=0){
+            var menu = """
+                    1 - Buscar libro por titulo
+                    2 - Listar libros registrados
+                    3 - Listar autores registrado
+                    4 - Listar autores vivos de un determinando año
+                    5 - listar libros por idima
+                    0 - Salir
+                    """;
+            System.out.println(menu);
+            option = reading.nextInt();
+            reading.nextLine();
+            switch (option){
+                case 1:
+                    getDataBook();
+                    break;
+                case 2:
+                    System.out.println("opcion 2");
+                    break;
 
 
-        //Top 10 libros mas descargados
-        System.out.println("Top 10 libros mas descargados");
-        data.booksList().stream()
-                .sorted(Comparator.comparing(DataBooks::download_count).reversed())
-                .limit(10)
-                .map(b -> b.title().toUpperCase())
-                .forEach(System.out::println);
-
-
-        //busqueda de libros por nombre
-            System.out.println("Ingrese el titulo del libro que desea buscar");
-            var titleBook = reading.nextLine();
-            json = apiRequest.getData(URL_BASE+"?search="+titleBook.replace(" ", "+"));
-            var searchData = converter.getData(json, Data.class);
-            Optional<DataBooks> searchedBook = searchData.booksList().stream()
-                    .filter(b ->b.title().toUpperCase().contains(titleBook.toUpperCase()))
-                    .findFirst();
-            if (searchedBook.isPresent()){
-                System.out.println("Libro encontrado");
-                System.out.println(searchedBook.get());
-            }else {
-                System.out.println("Libro no encontrado");
+                default:
+                    System.out.println("Opción invalida");
             }
 
-            //estadisticas
-        DoubleSummaryStatistics sta = data.booksList().stream()
-                .filter(d -> d.download_count() > 0)
-                .collect(Collectors.summarizingDouble(DataBooks::download_count));
-        System.out.println("Cantidad media de descargas: " +sta.getAverage() +
-        " Cantidad máxima de descargas: " + sta.getMax() +
-        " Cantidad mínima de descargas: "+sta.getMin() +
-        " Cantidad de registros evaluados "+sta.getCount()
-        );
+        }
+
+
+
+//        //Top 10 libros mas descargados
+//        System.out.println("Top 10 libros mas descargados");
+//        data.booksList().stream()
+//                .sorted(Comparator.comparing(DataBooks::download_count).reversed())
+//                .limit(10)
+//                .map(b -> b.title().toUpperCase())
+//                .forEach(System.out::println);
+//
+//
+//
+//
+//            //estadisticas
+//        DoubleSummaryStatistics sta = data.booksList().stream()
+//                .filter(d -> d.download_count() > 0)
+//                .collect(Collectors.summarizingDouble(DataBooks::download_count));
+//        System.out.println("Cantidad media de descargas: " +sta.getAverage() +
+//        " Cantidad máxima de descargas: " + sta.getMax() +
+//        " Cantidad mínima de descargas: "+sta.getMin() +
+//        " Cantidad de registros evaluados "+sta.getCount()
+//        );
+
+
+
+    }
+
+    // busca libros en la api
+    private Data getDataBook(){
+        System.out.println("Ingrese el titulo del libro que desea buscar");
+        var titleBook = reading.nextLine();
+        var json = apiRequest.getData(URL_BASE+"?search="+titleBook.replace(" ", "+"));
+        var data = converter.getData(json, Data.class);
+
+        Optional<DataBooks> searchedBook = data.booksList().stream()
+                .filter(b ->b.title().toUpperCase().contains(titleBook.toUpperCase()))
+                .findFirst();
+        if (searchedBook.isPresent()){
+            //System.out.println("Libro encontrado");
+            System.out.println(searchedBook.get());
+        }else {
+            System.out.println("Libro no encontrado");
+        }
+        return data;
 
     }
 }
